484e04f0f379dfc65633913801b661d9d69a4ca6
tmk2109 <khoitm@relipasoft.com>
2024-10-15 10:54:32 +0000

    BE todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user/user.entity";
+import { Task } from "./task/task.entity";
+import { UserModule } from "./user/user.module";
+import { TaskModule } from "./task/task.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1',
+      database: 'cms',
+      entities: [User, Task],
+      migrations: ['dist/migrations/*.js'],
+      synchronize: false,
+    }),
+    UserModule,
+    TaskModule,
+  ],
 })
+
 export class AppModule {}
